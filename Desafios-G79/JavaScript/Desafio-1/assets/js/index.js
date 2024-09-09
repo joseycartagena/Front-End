d985884801c38aeb09c7f3526a78dfaa98511d8d
joseycartagena <jose.cartagena.c@gmail.com>
2024-09-09 00:01:32 -0300

    commit desafio 1 js

--- Desafios-G79/JavaScript/Desafio-1/assets/js/index.js
+++ Desafios-G79/JavaScript/Desafio-1/assets/js/index.js
@@ -2,3 +2,8 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+valor = 0
+
+valorSpan = document.querySelector(".valor-total");
+valorSpan.innerHTML = valor
